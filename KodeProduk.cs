d7d6b6954441bbf71f2cc14c4e49dd8688d712bd
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-11 06:55:05 +0700

    menambahkan class KodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,20 @@
+﻿class KodeProduk
+{
+    private static readonly Dictionary<string, string> kodeProduk = new Dictionary<string, string>
+    {
+        { "Laptop", "E100" },
+        { "Smartphone", "E101" },
+        { "Tablet", "E102" },
+        { "Headset", "E103" },
+        { "Keyboard", "E104" },
+        { "Mouse", "E105" },
+        { "Printer", "E106" },
+        { "Monitor", "E107" },
+        { "SmartWatch", "E108" },
+        { "Kamera", "E109" }
+    };
+    public static string GetKodeProduk(string namaProduk)
+    {
+        return kodeProduk[namaProduk];
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class program
+{
+    static void Main()
+    {
+        Console.WriteLine("Masukkan Nama Barang: ");
+        string namaProduk = Console.ReadLine();
+        Console.WriteLine("Kode Produk: " + KodeProduk.GetKodeProduk(namaProduk));
+    }
+}
