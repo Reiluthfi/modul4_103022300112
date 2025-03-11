71846279443577baf135b3960b38d0a503d51c3e
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-11 07:36:21 +0700

    menambahkan class FanLaptop

---
+++ FanLaptop.cs
@@ -0,0 +1,37 @@
+﻿class FanLaptop
+{
+    private enum State { Quiet, Balanced, Performance, Turbo }
+    private State currentState;
+
+    public FanLaptop()
+    {
+        currentState = State.Quiet;
+        Console.WriteLine("Fan Laptop Mode Quiet");
+    }
+    public void setMode(string mode)
+    {
+        switch (mode)
+        {
+            case "Quiet":
+                Console.WriteLine("Fan Mode " + currentState + " Berubah Menjadi Mode Quiet");
+                currentState = State.Quiet;
+                break;
+            case "Balanced":
+                Console.WriteLine("Fan Mode " + currentState + " Berubah Menjadi Mode Balanced");
+                currentState = State.Balanced;
+                break;
+            case "Performance":
+                Console.WriteLine("Fan Mode " + currentState + " Berubah Menjadi Mode Performance");
+                currentState = State.Performance;
+                break;
+            case "Turbo":
+                Console.WriteLine("Fan Mode " + currentState + " Berubah Menjadi Mode Turbo");
+                currentState = State.Turbo;
+                break;
+            default:
+                Console.WriteLine("Fan Laptop Mode Tidak Ada");
+                break;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -5,5 +5,15 @@
         Console.WriteLine("Masukkan Nama Barang: ");
         string namaProduk = Console.ReadLine();
         Console.WriteLine("Kode Produk: " + KodeProduk.GetKodeProduk(namaProduk));
+
+        FanLaptop fan = new FanLaptop();
+        Console.WriteLine("\nSimulasi Mode Fan");
+        fan.setMode("Balanced");
+        fan.setMode("Performance");
+        fan.setMode("Turbo");
+        fan.setMode("Quiet");
+        fan.setMode("Ekstra");
+
     }
+
 }
